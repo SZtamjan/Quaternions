b2e0fd8343c890a8a8ae34d242f7092de400a68a
Damian Buczek <88405026+SZtamjan@users.noreply.github.com>
2023-05-21 11:21:03 +0200

    I can spin the valve/lever

--- Quaternions/Assets/Handel.cs
+++ Quaternions/Assets/Handel.cs
@@ -1,28 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Handel : MonoBehaviour
 {
     public Transform target;
-    
+    public Camera mainCamera;
     private void Update()
     {
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 cursorPos = Input.mousePosition;
         
-        Vector3 cursorPos = (mouseRay.origin + mouseRay.direction);
-        Vector3 relPos = new Vector3(cursorPos.x, cursorPos.y,cursorPos.z);
-        //Debug.Log(Input.mousePosition);
-        
+        Vector3 relPos = mainCamera.ScreenToWorldPoint(new Vector3(cursorPos.x, cursorPos.y, transform.position.z - mainCamera.transform.position.z));
         if (Input.anyKeyDown)
         {
-            target.position = cursorPos;
-            Debug.Log("kursor: " + cursorPos);
-            Debug.Log("Dzwignia" + transform.position);
-        }
+            Debug.Log("World pos: " + relPos);
+            target.position = relPos;
+        }    
         
-        relPos = new Vector3(relPos.x, relPos.y, 0);
-        transform.rotation = Quaternion.LookRotation(relPos,new Vector3(0,0,0));
+        float angle = (Mathf.Atan2(relPos.x, relPos.y) * Mathf.Rad2Deg)*-1f;
+        
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle);
+        Debug.DrawLine(transform.position, relPos, Color.magenta, Time.deltaTime, false);
+        
+        transform.rotation = targetRotation;
     }
 }
